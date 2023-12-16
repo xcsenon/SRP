eaa9e87d9e143dacf0bb87086f704c0f1d3fd908
vadim <vadim@XCSENON>
2023-12-16 17:51:47 +0200

    Добавьте файлы проекта.

---
+++ SRP/SRP.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+
+
+class BadExample {
+public:
+
+    void readData() {
+        std::cout << "Enter data: ";
+        std::cin >> data;
+    }
+
+    void processData() {
+        
+        data += " Processed";
+
+        
+        std::cout << "Processed data: " << data << std::endl;
+    }
+
+private:
+    std::string data;
+};
+
+
+class GoodExample {
+public:
+    
+    void readData() {
+        std::cout << "Enter data: ";
+        std::cin >> data;
+    }
+
+    
+    void processData() {
+        
+        data += " Processed";
+    }
+
+    
+    void printData() {
+        std::cout << "Processed data: " << data << std::endl;
+    }
+
+private:
+    std::string data;
+};
+
+int main() {
+    
+    BadExample badObj;
+    badObj.readData();
+    badObj.processData();
+
+    
+    GoodExample goodObj;
+    goodObj.readData();
+    goodObj.processData();
+    goodObj.printData();
+
+    return 0;
+}
